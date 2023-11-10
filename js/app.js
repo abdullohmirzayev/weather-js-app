527ba83f31690d04b46e250acca6da2efaa7baf2
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-11-10 23:22:03 +0500

    added getData fuc

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ const details = document.getElementById("details");
 const weatherIcon = document.getElementById("weather-icon");
 const overlay = document.getElementById("overlay");
 
+
+// get weather
+const getWeather = async (city) => {
+  const data = await getData(city);
+  return data;
+};
+
+// get location
+changeLocation.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const cityName = changeLocation.city.value.trim();
+  changeLocation.reset();
+  getWeather(cityName).then((data) => updateUI(data));
+});
--- js/forecast.js
+++ js/forecast.js
@@ -4,7 +4,7 @@ const KEY = "96b947a45d33d7dc1c49af3203966408";
 
 const getData = async (city) => {
   const base = "https://api.openweathermap.org/data/2.5/weather";
-  const query = `q=${city}&units=metric&appid=${KEY}`;
+  const query = `?q=${city}&units=metric&appid=${KEY}`;
 
   const req = await fetch(base + query);
   const data = await req.json();
