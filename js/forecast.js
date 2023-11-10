7a771b8c2e6a3266b225af837a437a6c3689b133
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-11-10 22:44:03 +0500

    added getData fuc

--- js/forecast.js
+++ js/forecast.js
@@ -1,4 +1,12 @@
-const KEY = '96b947a45d33d7dc1c49af3203966408'
-// bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
-// Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
-// Aloqa: Telegram => @akror_web
+const KEY = "96b947a45d33d7dc1c49af3203966408";
+
+// request get data
+
+const getData = async (city) => {
+  const base = "https://api.openweathermap.org/data/2.5/weather";
+  const query = `q=${city}&units=metric&appid=${KEY}`;
+
+  const req = await fetch(base + query);
+  const data = await req.json();
+  return data;
+};
