ea24ccc15eca72f5393c3c8f547db22038a65785
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-11-10 20:47:22 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const changeLocation = document.getElementById('change-location')
+const card = document.getElementById('card')
+const details = document.getElementById('details')
+const weatherIcon = document.getElementById('weather-icon')
+const overlay = document.getElementById('overlay')
+
---
+++ js/forecast.js
@@ -0,0 +1,4 @@
+const KEY = '96b947a45d33d7dc1c49af3203966408'
+// bu mening kalitim. Agar o'zingiz olsangiz va ishlamasa buni ishlating.
+// Agar ishlamay qolsa unda 1 soatda so'rovlar soni limitdan oshib ketgan bo'ladi.
+// Aloqa: Telegram => @akror_web
